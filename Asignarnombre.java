9a24942713a1e1938c6699b1ea24ddee93986ed3
chumi1 <lm738463@gmail.com>
2024-05-22 11:54:50 -0300

    Add files via upload

---
+++ Asignarnombre.java
@@ -0,0 +1,11 @@
+public class Asignarnombre {
+    
+        public static void main(String[] args) {
+          
+            String nombre = "Ludmila Miranda";
+            System.out.println("Mi nombre completo es: " + nombre); 
+        }
+    }
+    
+    
+    
---
+++ Charstarea.java
@@ -0,0 +1,13 @@
+public class Charstarea {
+    
+    public static void main (String [] args) {
+        char a='C';
+        char b='A';
+        char c='J';
+        String d="A";
+            String e="S";
+                System.out.println(a + "" + b + "" + c + "" + d + "" + e);
+            }
+        
+    }
+
